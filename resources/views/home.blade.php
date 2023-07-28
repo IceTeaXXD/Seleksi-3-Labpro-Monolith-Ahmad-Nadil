d1c13081b3401252e7c7d86b3fc04d6fef1dce33
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-07-29 01:19:07 +0700

    style: icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -414,7 +414,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Monolith App</title>
+    <title>Home </title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">

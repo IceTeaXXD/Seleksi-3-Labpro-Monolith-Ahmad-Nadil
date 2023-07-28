4849ba886c69043a8d7d1f76a0e6b3434f82e923
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-07-28 13:30:10 +0700

    feat: docker

--- database/migrations/2023_07_25_121433_create_user_table.php
+++ database/migrations/2023_07_25_121433_create_user_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('user')) {
+            Schema::dropIfExists('user');
+        }
+        
         Schema::create('user', function (Blueprint $table) {
             $table->id();
             $table->string('username', 50)->unique();
